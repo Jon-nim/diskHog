ba02e16b746f9e34341581086da4d0828fc417a6
Jon <jonathan.morgan.computer@gmail.com>
2021-10-12 22:28:40 -0600

    Idk why this didnt save
    final solution

--- diskHog.js
+++ diskHog.js
@@ -3,39 +3,81 @@ const fs = require('fs')
 const filesize = require('filesize')
 
 const args = process.argv.slice(2)
+let currendDir ="./"
 
-let currendDir ="C:\\Users\\Jona\\OneDrive\\FALL2021"
-
-const getAllFiles = function(directoryPath, arrayOfFiles) {
+const getAllFiles = function(directoryPath, arrayOfFiles, size) {
     files = fs.readdirSync(directoryPath)
 
-    if (arrayOfFiles == null) {
-        arrayOfFiles = {}
+    if (arrayOfFiles.children == []) {
+        arrayOfFiles.children
     }
-
     for(file of files){
         if (fs.statSync(directoryPath + "\\" + file).isDirectory()) {
-            arrayOfFiles[file] = {}
-            //console.log(arrayOfFiles)
-            arrayOfFiles[file] = getAllFiles(directoryPath + "\\" + file, arrayOfFiles[file])
+            let tmp = getAllFiles(directoryPath + "\\" + file, {name:file, size:0, children:[]},)
+            tmp.size = calcSize(tmp)
+            arrayOfFiles.children.push(tmp)
+
             
         } else{
-            arrayOfFiles[file] = {}
-            arrayOfFiles[file].size = filesize(fs.statSync(directoryPath + "\\" + file).size)
-            arrayOfFiles[file]["filename"] = file
+            let tmp = {
+                name :file,
+                size : fs.statSync(directoryPath + "\\" + file).size,
+                children : undefined
+            }
+            arrayOfFiles.children.push(tmp)
         }
     }
-        return arrayOfFiles
+    if(arrayOfFiles.name == ".\\"){
+        let tmptotal = 0
+        for(child of arrayOfFiles.children){tmptotal += child.size}
+        arrayOfFiles.size = tmptotal
+    }
+    return arrayOfFiles
+}
+const calcSize = function(root){
+    if(root.children == undefined){
+        return root.size
+    }
+    let totalSize = 0
+    for(child of root.children){
+        totalSize += calcSize(child)
+    }
+    return totalSize
+
+}
+const printToScreen = function(tree, tabs = 0){
+    numberOfTabs = ' '.repeat(tabs * 2)
+    console.log(`${numberOfTabs}(${filesize(tree.size)}) ${tree.name}`)
+    if(tree.children == undefined){return}
+    for(child of tree.children){
+        {printToScreen(child, tabs+1)}
+    }
 }
 
 
-const result1 = getAllFiles(currendDir)
-//console.log(result1.CS3380.Notes['Spikes']['readme.txt'])
-console.log(result1)
+let root = {
+    name: ".\\",
+    size: 0,
+    children : []
+}
 
-
-//TODO
-/*
--need to change the folder object to include size, name and children
--need to clean up data structure to look like notes1012
-*/
+if(args.length > 0){
+    if(args[0] == '-h' || args[0] == '--help'){
+        console.log(`
+        -p, --path value, the relative or absolute path name of a file or directory. Default is -p .
+        -s, --sort alpha | size
+        alpha sorts alphabetically (ascending)
+        size sorts by size (descending), default
+        -m, --metric, sizes displayed as KB, MB, GB, and TB.
+        -t, --threshold min, only displays files and folders of at least minimum size. min is the number of millions. Default is -t 1.
+        -h, --help prints this usage screen. Ignores all other arguments.
+        `)
+    }
+    else{
+        console.log('Not a valid argument')
+    }
+}
+else{
+    const result1 = getAllFiles(currendDir, root)
+    printToScreen(result1)
+}
