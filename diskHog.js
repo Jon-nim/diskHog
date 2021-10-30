f8e56a264bcacd82cb5a428c12f66d11ccb3844f
Jon <jonathan.morgan.computer@gmail.com>
2021-10-30 14:20:10 -0600

    fdsa
    fdsa

--- diskHog.js
+++ diskHog.js
@@ -1,9 +1,8 @@
 const path = require('path')
 const fs = require('fs')
 const filesize = require('filesize')
+const { mainModule } = require('process')
 
-const args = process.argv.slice(2)
-let currendDir ="./"
 
 const getAllFiles = function(directoryPath, arrayOfFiles, size) {
     files = fs.readdirSync(directoryPath)
@@ -54,30 +53,41 @@ const printToScreen = function(tree, tabs = 0){
     }
 }
 
+function main() {
 
-let root = {
-    name: ".\\",
-    size: 0,
-    children : []
-}
+    let root = {
+        name: ".\\",
+        size: 0,
+        children : []
+    }
+    
 
-if(args.length > 0){
-    if(args[0] == '-h' || args[0] == '--help'){
-        console.log(`
-        -p, --path value, the relative or absolute path name of a file or directory. Default is -p .
-        -s, --sort alpha | size
-        alpha sorts alphabetically (ascending)
-        size sorts by size (descending), default
-        -m, --metric, sizes displayed as KB, MB, GB, and TB.
-        -t, --threshold min, only displays files and folders of at least minimum size. min is the number of millions. Default is -t 1.
-        -h, --help prints this usage screen. Ignores all other arguments.
-        `)
+    const args = process.argv.slice(2)
+    let currendDir ="./"
+
+
+    if(args.length > 0){
+        if(args[0] == '-h' || args[0] == '--help'){
+            console.log(`
+            -p, --path value, the relative or absolute path name of a file or directory. Default is -p .
+            -s, --sort alpha | size
+            alpha sorts alphabetically (ascending)
+            size sorts by size (descending), default
+            -m, --metric, sizes displayed as KB, MB, GB, and TB.
+            -t, --threshold min, only displays files and folders of at least minimum size. min is the number of millions. Default is -t 1.
+            -h, --help prints this usage screen. Ignores all other arguments.
+            `)
+        }
+        else{
+            console.log('Not a valid argument')
+        }
     }
     else{
-        console.log('Not a valid argument')
+        const result1 = getAllFiles(currendDir, root)
+        printToScreen(result1)
     }
+    
 }
-else{
-    const result1 = getAllFiles(currendDir, root)
-    printToScreen(result1)
-}
+
+main();
+
