07d435f7ff8e9926e2a3c433936921e61c4d58a5
Jon <jonathan.morgan.computer@gmail.com>
2021-10-30 15:57:33 -0600

    making progress
    seperated them into files,
    done with:
    ---help
    ---lang

--- diskHog.js
+++ diskHog.js
@@ -2,56 +2,8 @@ const path = require('path')
 const fs = require('fs')
 const filesize = require('filesize')
 const { mainModule } = require('process')
-
-
-const getAllFiles = function(directoryPath, arrayOfFiles, size) {
-    files = fs.readdirSync(directoryPath)
-
-    if (arrayOfFiles.children == []) {
-        arrayOfFiles.children
-    }
-    for(file of files){
-        if (fs.statSync(directoryPath + "\\" + file).isDirectory()) {
-            let tmp = getAllFiles(directoryPath + "\\" + file, {name:file, size:0, children:[]},)
-            tmp.size = calcSize(tmp)
-            arrayOfFiles.children.push(tmp)
-
-            
-        } else{
-            let tmp = {
-                name :file,
-                size : fs.statSync(directoryPath + "\\" + file).size,
-                children : undefined
-            }
-            arrayOfFiles.children.push(tmp)
-        }
-    }
-    if(arrayOfFiles.name == ".\\"){
-        let tmptotal = 0
-        for(child of arrayOfFiles.children){tmptotal += child.size}
-        arrayOfFiles.size = tmptotal
-    }
-    return arrayOfFiles
-}
-const calcSize = function(root){
-    if(root.children == undefined){
-        return root.size
-    }
-    let totalSize = 0
-    for(child of root.children){
-        totalSize += calcSize(child)
-    }
-    return totalSize
-
-}
-const printToScreen = function(tree, tabs = 0){
-    numberOfTabs = ' '.repeat(tabs * 2)
-    console.log(`${numberOfTabs}(${filesize(tree.size)}) ${tree.name}`)
-    if(tree.children == undefined){return}
-    for(child of tree.children){
-        {printToScreen(child, tabs+1)}
-    }
-}
+const tags = require('./diskHogTags')
+const getfiles = require('./diskHogGetFiles')
 
 function main() {
 
@@ -60,32 +12,16 @@ function main() {
         size: 0,
         children : []
     }
-    
 
+    let lang = "en_US"
     const args = process.argv.slice(2)
     let currendDir ="./"
 
+    lang = tags.changeLang(args)
+    tags.help(args, lang)
+    const result1 = getfiles.getAllFiles(currendDir, root)
+    getfiles.printToScreen(result1)
 
-    if(args.length > 0){
-        if(args[0] == '-h' || args[0] == '--help'){
-            console.log(`
-            -p, --path value, the relative or absolute path name of a file or directory. Default is -p .
-            -s, --sort alpha | size
-            alpha sorts alphabetically (ascending)
-            size sorts by size (descending), default
-            -m, --metric, sizes displayed as KB, MB, GB, and TB.
-            -t, --threshold min, only displays files and folders of at least minimum size. min is the number of millions. Default is -t 1.
-            -h, --help prints this usage screen. Ignores all other arguments.
-            `)
-        }
-        else{
-            console.log('Not a valid argument')
-        }
-    }
-    else{
-        const result1 = getAllFiles(currendDir, root)
-        printToScreen(result1)
-    }
     
 }
 
